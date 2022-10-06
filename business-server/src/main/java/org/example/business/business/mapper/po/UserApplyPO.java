e86512f9bffbdf47a733519a8face3ef5b4cd35a
xin.huang6 <xin.huang6@zhenai.com>
2022-10-06 11:10:16 +0800

    申请表主键自增

--- business-server/src/main/java/org/example/business/business/mapper/po/UserApplyPO.java
+++ business-server/src/main/java/org/example/business/business/mapper/po/UserApplyPO.java
@@ -1,5 +1,7 @@
 package org.example.business.business.mapper.po;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -13,6 +15,7 @@ import java.util.Date;
 @Data
 @TableName("user_apply")
 public class UserApplyPO {
+    @TableId(value = "id",type = IdType.AUTO)
     private Integer id;
     private Integer userId;
     private Integer receiveId;
